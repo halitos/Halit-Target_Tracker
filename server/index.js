83ccd41bd17f258ebb6ee8147405814ee83975a1
halit <oskan96@gmail.com>
2021-02-25 09:24:51 +0000

    fixed static path

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const db = require("./connection");
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get("/api", (req, res) => {
   res.json({
@@ -18,14 +18,14 @@ app.get("/student_email", function (req, res) {
     .catch((err) => res.status(500).json({ error: err }));
 });
 
-app.listen(PORT || process.env.PORT, function () {
-  console.log("Server is listening on port 3001. Ready to accept requests!");
-});
-
 // For Deployment
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log("Server is listening on port 3001. Ready to accept requests!");
 });
